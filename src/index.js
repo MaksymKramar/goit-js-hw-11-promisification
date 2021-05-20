1eb32ea3e7c2aefe0942be9a0f1d269aa454fa39
Maksym Kramar <maxim.kramar77@gmail.com>
2021-05-20 09:27:04 +0200

    Исправляет 1 задание

--- src/index.js
+++ src/index.js
@@ -3,19 +3,19 @@
 // через ms миллисекунд.Значением исполнившегося промиса должно быть то кол - во миллисекунд
 // которое передали во время вызова функции delay.
 
-const delay = ms => {
-    console.log(ms)
-    return Promise.resolve(setTimeout(() => ms, ms));   
-};
-
 // const delay = ms => {
-
-//     return new Promise((resolve) => {
-//         setTimeout(() => {
-//             resolve(ms);           
-//   }, ms);
-//     })   
+//     console.log(ms)
+//     return Promise.resolve(setTimeout(() => ms, ms));   
 // };
+
+const delay = ms => {
+
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(ms);
+    }, ms);
+  })
+};
 
 const logger1 = time => console.log(`Resolved after ${time} ms`);
 
@@ -39,13 +39,13 @@ const toggleUserState = (allUsers, userName) => {
   const updatedUsers = allUsers.map(user =>
     user.name === userName ? { ...user, active: !user.active } : user,
   );
-    
-    return Promise.resolve(updatedUsers);
 
-    // return new Promise((resolve, reject) => {
-    //     resolve(updatedUsers);        
-    //     reject('erro')    
-    // })
+  return Promise.resolve(updatedUsers);
+
+  // return new Promise((resolve, reject) => {
+  //     resolve(updatedUsers);        
+  //     reject('erro')    
+  // })
 };
 
 const logger2 = updatedUsers => console.table(updatedUsers);
@@ -72,21 +72,21 @@ const randomIntegerFromInterval = (min, max) => {
 };
 
 const makeTransaction = (transaction) => {
-    const delay = randomIntegerFromInterval(200, 500);
-    
-    return new Promise((resolve, reject) => {
-        const canProcess = Math.random() > 0.3;
-        setTimeout(() => {
-            if (canProcess) {
-                resolve({id: transaction.id, time: delay });
-            }
-            reject(transaction.id);
-        }, delay);
-    });
+  const delay = randomIntegerFromInterval(200, 500);
+
+  return new Promise((resolve, reject) => {
+    const canProcess = Math.random() > 0.3;
+    setTimeout(() => {
+      if (canProcess) {
+        resolve({ id: transaction.id, time: delay });
+      }
+      reject(transaction.id);
+    }, delay);
+  });
 };
 
-const logSuccess = ({id, time}) => {
- console.log(`Transaction ${id} processed in ${time} ms`);
+const logSuccess = ({ id, time }) => {
+  console.log(`Transaction ${id} processed in ${time} ms`);
 };
 
 const logError = id => {
